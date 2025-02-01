680496af67c5f12cf75250dfef20ab31355eb2f3
Paolo Ortalda <paoloortalda@gmail.com>
2025-02-01 19:16:46 +0100

    ok

---
+++ test-3/test3/src/components/SezioneImpresa.jsx
@@ -0,0 +1,11 @@
+export default function SezioneImpresa() {
+
+    return(
+        <>
+        <h2>questi dati li compila l'impresa</h2>
+        <p>camera di commercio</p>
+        <p>P.I.</p>
+        
+        </>
+    )
+}
---
+++ test-3/test3/src/components/SezionePersona.jsx
@@ -0,0 +1,10 @@
+export default function SezionePersona() {
+
+    return(
+        <>
+        <h2>questi dati li compila la persona</h2>
+        <p>passaporto</p>
+        <p>C.F.</p>
+        </>
+    )
+}
