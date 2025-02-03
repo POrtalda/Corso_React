916b8e92e9e67c50f1c3abcadd81e52886dd17c8
Paolo Ortalda <paoloortalda@gmail.com>
2025-02-03 22:42:15 +0100

    ok

--- esercizi/lezione_3/libreria/src/components/Card.jsx
+++ esercizi/lezione_3/libreria/src/components/Card.jsx
@@ -1,13 +1,17 @@
 import './Card.css'
-import Libro from './Libro'
+
+
 
 export default function Card(props){
-    const cardStyleDisponibile ={display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: } 
-    background-color: rgb(195, 199, 199);
-    margin: 20px;
+    const cardStyleDisponibile ={display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#c3c7c7", margin: "20px"} 
+    const cardStyleNONDisponibile ={display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#ca4b4b", margin: "20px"} 
+    
+    
+   
     return(
+        
         <>
-        <div style={}>
+        <div style={cardStyleDisponibile}>
             {props.children}
         </div>
         
