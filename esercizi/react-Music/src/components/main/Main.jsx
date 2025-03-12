bc4ea57508a7bcd596ce6301df109d6a2dfd2af2
POrtalda <paoloortalda@gmail.com>
2025-03-12 17:20:32 +0100

    ok

--- esercizi/react-Music/src/components/main/Main.jsx
+++ esercizi/react-Music/src/components/main/Main.jsx
@@ -13,8 +13,8 @@ export default function Main({ isDarkModeActive,
           <h4 id='leTuePlaylist'>le tue playlist:</h4>
           <div className='card-container'>
             {
-              filteredPlaylists.lenght == 0 ? (
-                  console.log('playlist non trovata')
+              filteredPlaylists.length == 0 ? (
+                 <p>playlist non trovata</p>
               ) : (
                 filteredPlaylists.map(playlist => (
                   <PlaylistCard key={playlist.id} playlist={playlist} />
