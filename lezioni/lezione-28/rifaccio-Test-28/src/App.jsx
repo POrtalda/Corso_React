f1ca56896275d28aa59eeadb59bd90435a2472e5
POrtalda <paoloortalda@gmail.com>
2025-07-01 21:13:40 +0200

    rifaccio test 28

--- lezioni/lezione-28/rifaccio-Test-28/src/App.jsx
+++ lezioni/lezione-28/rifaccio-Test-28/src/App.jsx
@@ -7,8 +7,15 @@ function App() {
   const API_URL = 'https://portalda.github.io/movies_fake_api/movies.json'
   const [movies, setMovies] = useState(null)
 
-  useEffect(()=> {
-    getAPIMovies()
+  useEffect(() => {
+    // verifico se ci sono film salvati nel LocalStorage
+    const savedMovies = localStorage.getItem('movies')
+    if (savedMovies) {
+      // se ci sono film salvati, li recupero e li imposto nello stato
+      setMovies(JSON.parse(savedMovies))
+    } else {
+      getAPIMovies()
+    }
   }, [])
 
   function getAPIMovies() {
@@ -18,10 +25,37 @@ function App() {
         //console.log(data)
 
         // aggiorno lo stato setMovies con i dati della nostra API
-        setMovies(data)    
+        //setMovies(data)  
+        // in ogni oggetto aggiungi anche la prop isFavorite e impostala a false
+        const newMovies = data.map(movie => (
+          { ...movie, isFavorite: false }
+        ))
+
+        console.log(newMovies)
+
+        // aggiorno lo stato con il nuovo array con l'aggiunta di isFavorite
+        setMovies(newMovies)
+
+         // salva anche nel local storage
+      localStorage.setItem('movies', JSON.stringify(newMovies));
+
+        
       })
   }
 
+  // creo una funzione per attivare i preferiti con la quale 
+  // riconosco l'id del film scelto
+  function toggleFavorite(id) {
+    const updatedMovies = movies.map(movie =>
+      movie.id === id
+        ? { ...movie, isFavorite: !movie.isFavorite }
+        : movie
+    )
+    setMovies(updatedMovies)
+
+    // salvo i film aggiornati nel LocalStorage
+    localStorage.setItem('movies', JSON.stringify(updatedMovies)) 
+  }
   return (
     <>
       <h1>ReactFilm Library 🎦</h1>
@@ -31,7 +65,7 @@ function App() {
       componente film*/}
       {movies !== null ? (
         movies.map(movie => (
-          <Film key={movie.id} movie={movie} />
+          <Film key={movie.id} movie={movie} toggleFavorite={toggleFavorite} />
         ))
       ) : (
         <p>Loading...</p>
--- lezioni/lezione-28/rifaccio-Test-28/src/components/Film/Film.jsx
+++ lezioni/lezione-28/rifaccio-Test-28/src/components/Film/Film.jsx
@@ -1,6 +1,7 @@
 import './Film.css';
+import { FaRegHeart, FaHeart } from "react-icons/fa6";
 
-export default function Film({ movie }) {
+export default function Film({ movie, toggleFavorite }) {
 
     return (
         <>
@@ -10,13 +11,23 @@ export default function Film({ movie }) {
                     <img src={movie.img_url} alt={movie.title} />
                 </div>
 
+                <div className='like-movie' onClick={() => {
+                    toggleFavorite(movie.id);
+                }}>
+                    {movie.isFavorite ? (
+                        <FaHeart className='icon-heart' />
+                    ) : (
+                        <FaRegHeart className='icon-heart' />
+                    )}
+                </div>
+
                 <div className='info-movie'>
                     <h2>{movie.title}</h2>
                     <h3>{movie.directed_by}</h3>
                     <p>Anno: [{movie.year}] Durata:{movie.duration}"</p>
-                    <p>{movie.genres.map(genre => (<span>{genre},</span>))}</p>
+                    <p>{movie.genres.map((genre, index) => (<span key={index}> {genre}</span>))}</p>
                     <br />
-                    <hr /> 
+                    <hr />
                     <br />
                     <p>{movie.short_description}</p>
                 </div>
