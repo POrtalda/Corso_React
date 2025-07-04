adbbe2365bfc37d51ee6ee1f6539edfc0af6057d
PAOLO ORTALDA <154626945+POrtalda@users.noreply.github.com>
2025-07-04 21:22:44 +0200

    lezione 30

--- lezioni/lezione-30/test-30/src/App.jsx
+++ lezioni/lezione-30/test-30/src/App.jsx
@@ -1,33 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Recipe from './components/Recipe/Recipe'
+
+const API_URL = 'https://portalda.github.io/fake-api-recipes/recipes.json';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipes, setRecipes] = useState(null)
+  const [showFavorite, setShowFavorite] = useState(false)
+
+  useEffect(() => {
+    callAPI();
+  }, [])
+
+  useEffect(() => {
+    if (recipes !== null) {
+
+      const favRecipes = recipes.filter(recipe => recipe.isFavorite)
+        .map(recipe => recipe.id)
+
+      console.log(favRecipes)
+
+      localStorage.setItem('fav-recipes', JSON.stringify(favRecipes))
+    }
+
+  }, [recipes])
+
+  function callAPI() {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        //console.log(data)
+
+        // leggi dal localstorage
+        const favRecipes = JSON.parse(localStorage.getItem('fav-recipes')) || [];
+        
+        // IMPORTANTE!!!!!
+        // inserire in setRecipes anche isFavorite
+        // creiamo un nuovo array da mettere nello stato recipes
+        // questo array cicla i dati che arrivano dall'API e per ogni ricetta
+        // aggiunge la proprietà isFavorite e la mette a true se è 
+        // contenuta in favRecipes altrimenti la valorizza a false 
+        const newRecipes = data.map(recipe => (
+          { ...recipe, isFavorite: favRecipes.includes(recipe.id) }
+        ))
+
+        setRecipes(newRecipes)
+        console.log({ newRecipes })
+
+      })
+  }
+
+  function handleUpdateRecipe(updateRecipe) {
+    //alert(`hai cliccato la stellina della ricetta con id ${recipe.id}`)
+
+    // qui aggiorno isFavorite
+    const newRecipe = { ...updateRecipe, isFavorite: !updateRecipe.isFavorite }
+
+    // aggiorno tutto lo stato recipes
+    setRecipes(recipes.map(updateRecipe => (
+      updateRecipe.id === newRecipe.id ? newRecipe : updateRecipe)))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>🥧 Ricette Reactose 🍝</h1>
+
+      <button onClick={() => setShowFavorite(!showFavorite)}>
+        {showFavorite ? (
+          'tutte le ricette'
+        ): (
+          'preferite'
+        )}
+      </button>
+
+      {recipes !== null ? (
+
+        // qui c'è la logica del button per vedere solo le ricette preferite!!!
+        // se showFavorite è true prendo solo le ricette preferite
+        // altrimenti prendile tutte
+        (showFavorite ? recipes.filter(recipe => recipe.isFavorite) : recipes)
+        .map(recipe => (
+          <Recipe key={recipe.id} recipe={recipe} updateRecipe={handleUpdateRecipe} />
+        ))
+      ) : (
+        <p>Caricamento delle ricette in corso ...</p>
+      )}
+
     </>
   )
 }
---
+++ lezioni/lezione-30/test-30/src/components/Recipe/Recipe.jsx
@@ -0,0 +1,30 @@
+import './Recipe.css';
+import { IoIosStar, IoIosStarOutline } from "react-icons/io";
+
+export default function Recipe({ recipe,updateRecipe }) {
+
+    return (
+        <>
+            <div className='card-recipe'>
+                <h2>{recipe.title}</h2>
+                <img src={recipe.photo} alt={recipe.title} />
+
+                <div  onClick={() => updateRecipe(recipe)}>
+                    {recipe.isFavorite ? (
+                        <IoIosStar className='icon'/>
+                    ) : (
+                        <IoIosStarOutline className='icon'/>
+                    )}
+                    
+                    
+                </div>
+
+                <p>di {recipe.author}</p>
+                <p>⌛ {recipe.prep_time}</p>
+                <p>{recipe.cuisine_type}</p>
+                <p>{recipe.description}</p>
+
+            </div>
+        </>
+    );
+}
